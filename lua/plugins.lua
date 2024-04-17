3766b5a0b224a96bceb0fde05c0812877f4d92c7
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2024-04-17 19:48:35 -0400

    Add keybindings to open lazygit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -419,6 +419,32 @@ if enable_copilot then
   }, { prefix = "<leader>" })
 end
 -- }}}
+
+-- lazygit Terminal {{{
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  hidden = true,
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "i", "<C-g>", "<cmd>close<CR>", {noremap = true, silent = true})
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<C-g>", "<cmd>close<CR>", {noremap = true, silent = true})
+    vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-g>", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.keymap.set("i", "<C-g>", "<cmd>lua _lazygit_toggle()<CR>")
+vim.keymap.set("n", "<C-g>", "<cmd>lua _lazygit_toggle()<CR>")
+vim.keymap.set("t", "<C-g>", "<cmd>lua _lazygit_toggle()<CR>")
+-- }}}
 -- }}}
 
 -- vim:set foldenable:
