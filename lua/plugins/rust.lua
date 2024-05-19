c0936f4bad51c70879ff837579475147a7ab7564
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2024-05-19 16:23:45 -0400

    Autofocus crates.nvim popups

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -3,7 +3,11 @@ return {
     "saecki/crates.nvim",
     cond = vim.g.enable_crates_nvim == 1,
     tag = "stable",
-    config = true,
+    opts = {
+      popup = {
+        autofocus = true,
+      }
+    }
   },
 
   {
