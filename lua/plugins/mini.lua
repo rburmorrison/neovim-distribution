2d788c7825d06de0ceef61722dc551da9af17e57
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2024-08-14 16:42:50 -0400

    Remove mini.pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
     "echasnovski/mini.nvim",
     config = function()
       require("mini.align").setup()
-      require("mini.pairs").setup()
       require("mini.comment").setup()
     end,
   },
