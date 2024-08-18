8bbdc7444800920cea0c295aebd48a7e7d95dd6e
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2024-08-18 14:03:53 -0400

    Remove Clojure plugins and add back mini.pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,6 +3,7 @@ return {
     "echasnovski/mini.nvim",
     config = function()
       require("mini.align").setup()
+      require("mini.pairs").setup()
       require("mini.comment").setup()
     end,
   },
