3d5492a0fa3aafb16ff644277ab4632673674614
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2024-08-30 21:34:58 -0400

    Add cmdline sources to nvim-cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -117,6 +117,23 @@ return {
         },
       })
 
+      cmp.setup.cmdline({ "/", "?", }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer", },
+        },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path", },
+        }, {
+          { name = "cmdline", },
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false, },
+      })
+
       --------------------------
       -- Border Configuration --
       --------------------------
