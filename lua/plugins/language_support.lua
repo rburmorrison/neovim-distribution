0f9061ec0e49221b2822075964670a986cf7c7b3
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2024-08-31 11:20:48 -0400

    Add bashls to ensure_installed list

--- lua/plugins/language_support.lua
+++ lua/plugins/language_support.lua
@@ -191,6 +191,7 @@ return {
       require("mason").setup()
       require("mason-lspconfig").setup({
         ensure_installed = {
+          "bashls",
           "cssls",
           "emmet_language_server",
           "html",
