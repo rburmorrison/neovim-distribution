61a3de46e7334842bc5c5a4bc8a283ea00d17031
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2024-11-26 13:32:53 -0500

    feat: add the hurl.nvim plugin to make cURL requests

--- lua/plugins/essential.lua
+++ lua/plugins/essential.lua
@@ -54,4 +54,38 @@ return {
       { "<leader>oo", "<cmd>lua require('oil').toggle_float()<cr>", desc = "Open Oil", },
     },
   },
+
+  {
+    "jellydn/hurl.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    ft = "hurl",
+    opts = {
+      debug = false,
+      show_notification = false,
+      mode = "popup",
+      formatters = {
+        json = { "jq", }, -- Make sure you have install jq in your system, e.g: brew install jq
+      },
+      mappings = {
+        close = "q",          -- Close the response popup or split view
+        next_panel = "<C-n>", -- Move to the next response popup window
+        prev_panel = "<C-p>", -- Move to the previous response popup window
+      },
+    },
+    keys = {
+      -- Run API request
+      { "<leader>hR", "<cmd>HurlRunner<CR>",        desc = "Run all requests", },
+      { "<leader>hr", "<cmd>HurlRunnerAt<CR>",      desc = "Run current request", },
+      { "<leader>hA", "<cmd>HurlRunnerToEntry<CR>", desc = "Run requests above", },
+      { "<leader>hB", "<cmd>HurlRunnerToEnd<CR>",   desc = "Run requests below", },
+      { "<leader>hm", "<cmd>HurlToggleMode<CR>",    desc = "Hurl Toggle Mode", },
+      { "<leader>hv", "<cmd>HurlVerbose<CR>",       desc = "Run request in verbose mode", },
+      { "<leader>hV", "<cmd>HurlVeryVerbose<CR>",   desc = "Run API in very verbose mode", },
+      { "<leader>hr", ":HurlRunner<CR>",            desc = "Hurl Runner",                  mode = "v", },
+    },
+  },
 }
