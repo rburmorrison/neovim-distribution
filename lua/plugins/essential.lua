88b9c5a614d70e46a562f19a8186513a00c6d0d7
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2024-11-26 13:44:03 -0500

    fix: adjust hurl.nvim bindings to only be active in hurl files

--- lua/plugins/essential.lua
+++ lua/plugins/essential.lua
@@ -62,30 +62,37 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
-    ft = "hurl",
-    opts = {
-      debug = false,
-      show_notification = false,
-      mode = "popup",
-      formatters = {
-        json = { "jq", }, -- Make sure you have install jq in your system, e.g: brew install jq
-      },
-      mappings = {
-        close = "q",          -- Close the response popup or split view
-        next_panel = "<C-n>", -- Move to the next response popup window
-        prev_panel = "<C-p>", -- Move to the previous response popup window
-      },
-    },
-    keys = {
-      -- Run API request
-      { "<leader>hR", "<cmd>HurlRunner<CR>",        desc = "Run all requests", },
-      { "<leader>hr", "<cmd>HurlRunnerAt<CR>",      desc = "Run current request", },
-      { "<leader>hA", "<cmd>HurlRunnerToEntry<CR>", desc = "Run requests above", },
-      { "<leader>hB", "<cmd>HurlRunnerToEnd<CR>",   desc = "Run requests below", },
-      { "<leader>hm", "<cmd>HurlToggleMode<CR>",    desc = "Hurl Toggle Mode", },
-      { "<leader>hv", "<cmd>HurlVerbose<CR>",       desc = "Run request in verbose mode", },
-      { "<leader>hV", "<cmd>HurlVeryVerbose<CR>",   desc = "Run API in very verbose mode", },
-      { "<leader>hr", ":HurlRunner<CR>",            desc = "Hurl Runner",                  mode = "v", },
-    },
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "hurl",
+        group = vim.api.nvim_create_augroup("VisualSettings", {}),
+        callback = function()
+          require("hurl").setup({
+            debug = false,
+            show_notification = false,
+            mode = "popup",
+            formatters = {
+              json = { "jq", }, -- Make sure you have install jq in your system, e.g: brew install jq
+            },
+            mappings = {
+              close = "q",          -- Close the response popup or split view
+              next_panel = "<C-n>", -- Move to the next response popup window
+              prev_panel = "<C-p>", -- Move to the previous response popup window
+            },
+          })
+
+          require("which-key").add({
+            { "<leader>hR", "<cmd>HurlRunner<CR>",        buffer = true, desc = "Run all requests", },
+            { "<leader>hr", "<cmd>HurlRunnerAt<CR>",      buffer = true, desc = "Run current request", },
+            { "<leader>hA", "<cmd>HurlRunnerToEntry<CR>", buffer = true, desc = "Run requests above", },
+            { "<leader>hB", "<cmd>HurlRunnerToEnd<CR>",   buffer = true, desc = "Run requests below", },
+            { "<leader>hm", "<cmd>HurlToggleMode<CR>",    buffer = true, desc = "Hurl Toggle Mode", },
+            { "<leader>hv", "<cmd>HurlVerbose<CR>",       buffer = true, desc = "Run request in verbose mode", },
+            { "<leader>hV", "<cmd>HurlVeryVerbose<CR>",   buffer = true, desc = "Run API in very verbose mode", },
+            { "<leader>hr", ":HurlRunner<CR>",            buffer = true, desc = "Run Selected Requests",        mode = "v", },
+          })
+        end,
+      })
+    end,
   },
 }
