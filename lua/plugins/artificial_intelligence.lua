4092dd4ec63e8464835de4505e341d2417240ac7
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2024-12-05 14:47:13 -0500

    feat: replace CodeCompanion with Parrot plugin
    - Replaced the "olimorris/codecompanion.nvim" plugin with "frankroeder/parrot.nvim" plugin.
    - Updated dependencies to include "ibhagwan/fzf-lua" and removed "nvim-treesitter/nvim-treesitter".
    - Modified key mappings to accommodate Parrot plugin functionality including commands like PrtAppend, PrtPrepend, etc.
    - Introduced configuration setup using API key from environment variable and added hooks for generating commit messages.

--- lua/plugins/artificial_intelligence.lua
+++ lua/plugins/artificial_intelligence.lua
@@ -1,41 +1,47 @@
 return {
   {
-    "olimorris/codecompanion.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
+    "frankroeder/parrot.nvim",
+    dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim", },
     lazy = false,
     keys = {
-      { "<leader>ci", "<cmd>CodeCompanion<cr>",            desc = "CodeCompanion Inline Edit", },
-      { "<leader>ci", "<cmd>CodeCompanion<cr>",            desc = "CodeCompanion Inline Edit",    mode = "v", },
-      { "<leader>cc", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Toggle CodeCompanion Chat", },
-      { "<leader>ca", "<cmd>CodeCompanionActions<cr>",     desc = "Invoke CodeCompanion Actions", },
-      { "<leader>ca", "<cmd>CodeCompanionActions<cr>",     desc = "Invoke CodeCompanion Actions", mode = "v", },
+      { "<leader>pa", ":PrtAppend<CR>",     mode = { "n", "v", }, desc = "Parrot Append", },
+      { "<leader>pb", ":PrtPrepend<CR>",    mode = { "n", "v", }, desc = "Parrot Prepend", },
+      { "<leader>pi", ":PrtImplement<CR>",  mode = { "n", "v", }, desc = "Parrot Implement", },
+      { "<leader>pr", ":PrtRewrite<CR>",    mode = { "n", "v", }, desc = "Parrot Rewrite", },
+      { "<leader>pc", ":PrtChatToggle<CR>", mode = { "n", "v", }, desc = "Parrot Chat Toggle", },
+      { "<leader>py", ":PrtRetry<CR>",      mode = { "n", "v", }, desc = "Parrot Retry", },
+      { "<leader>pp", ":PrtChatPaste<CR>",  mode = { "n", "v", }, desc = "Parrot Chat Paste", },
+      { "<leader>pC", ":PrtCommit<CR>",     mode = { "n", },      desc = "Parrot Generate Commit Message", },
     },
-    opts = {
-      strategies = {
-        inline = { adapter = "openai", },
-        chat = {
-          adapter = "openai",
-          slash_commands = {
-            ["buffer"] = { opts = { provider = "telescope", }, },
-            ["file"] = { opts = { provider = "telescope", }, },
-            ["help"] = { opts = { provider = "telescope", }, },
-            ["symbols"] = { opts = { provider = "telescope", }, },
+    config = function()
+      require("parrot").setup {
+        providers = {
+          openai = {
+            api_key = os.getenv "OPENAI_API_KEY",
           },
         },
-        agent = { adapter = "openai", },
-        cmd = { adapter = "openai", },
-      },
-      display = {
-        action_palette = {
-          provider = "telescope",
+        hooks = {
+          Commit = function(prt, params)
+            local diff_output = vim.fn.system("git diff --no-ext-diff --staged")
+            local chat_prompt = string.format([[
+              You are an expert at the Conventional Commit format. Take the diff below, and
+              output a diff block of the suggested conventional commit block. Give no additional
+              output other than the resulting block. The only time you should add any extra words
+              is if the diff is empty. In that case, provide a short message for the user so they
+              know what happened. Perhaps suggest they should stage their changes first.
+
+              Here is the diff:
+
+              ```diff
+              %s
+              ```
+            ]], diff_output)
+
+            local model = prt.get_model("command")
+            prt.Prompt(params, prt.ui.Target.new, model, nil, chat_prompt)
+          end,
         },
-        diff = {
-          layout = "horizontal",
-        },
-      },
-    },
+      }
+    end,
   },
 }
