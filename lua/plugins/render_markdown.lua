1726122e17e13fc6d0a2a99ba6cc435f2be95cb0
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2025-02-05 17:12:33 -0500

    feat: add codecompanion filetype to render-markdown plugin

--- lua/plugins/render_markdown.lua
+++ lua/plugins/render_markdown.lua
@@ -2,7 +2,7 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", },
-    ft = { "markdown", },
+    ft = { "markdown", "codecompanion" },
     opts = {},
   },
 }
